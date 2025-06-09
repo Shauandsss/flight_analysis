4475eaaeb4ee3a5edc06dcb528147c9d18d07b06
Shauandsss <Shauandsss>
2025-06-09 09:10:20 +0000

    fix: bronze

--- src/bronze flights.py
+++ src/bronze flights.py
@@ -1,9 +1,6 @@
 # Databricks notebook source
-# Databricks notebook source
-df = spark.read.option("header", "true").csv("/Volumes/raw/flights/us_flights/")
 
-# Remove duplicates
-df = df.dropDuplicates()
+df = spark.read.option("header", "true").csv("/Volumes/raw/flights/us_flights/")
 
 # Write to the table
 df.coalesce(1).write.mode("overwrite").saveAsTable("bronze.flights.us_flights")
