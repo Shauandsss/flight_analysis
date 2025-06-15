0462505e5d217f212f76657e60b03c01993128f1
Shauandsss <Shauandsss>
2025-06-15 07:01:18 +0000

    fix: removing unnecessary comments and displays

--- src/bronze flights.py
+++ src/bronze flights.py
@@ -1,6 +1,4 @@
 # Databricks notebook source
-
 df = spark.read.option("header", "true").csv("/Volumes/raw/flights/us_flights/")
 
-# Write to the table
 df.coalesce(1).write.mode("overwrite").saveAsTable("bronze.flights.us_flights")
